86c9e1940544affc67e282fd0737ec3c6d681ea9
Utkarsh Narain <utkarshnarain007@gmail.com>
2021-04-06 16:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import numpy as np
 description = '''This is what I have been programmed to do'''
 client = commands.Bot(command_prefix='?', description=description,)
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -19,36 +20,40 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
-    async def on_message(msg):
-        for word in ['chut']:
-            if word in msg.content:
-                await msg.delete()
-		await client.process_commands(msg)
+async def on_message(msg):
+    for word in ['chut']:
+        if word in msg.content:
+            await msg.delete()
+    await client.process_commands(msg)
+
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def load(ctx, extension):
-	client.load_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
     # need logs here
-	# print(f'The {extension} is loaded by {ctx.author.name}') 
-	await ctx.send(f'The {extension} is successfully Loaded.')
+    # print(f'The {extension} is loaded by {ctx.author.name}')
+    await ctx.send(f'The {extension} is successfully Loaded.')
+
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
+    client.unload_extension(f'cogs.{extension}')
     # need logs here
-	# print(f'The {extension} is unloaded by {ctx.author.name}')
-	await ctx.send(f'The {extension} is successfully unloaded.')
+    # print(f'The {extension} is unloaded by {ctx.author.name}')
+    await ctx.send(f'The {extension} is successfully unloaded.')
 
 for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
-@client.command(aliases=['pong'],description='ping the bot mofo')
+
+@client.command(aliases=['pong'], description='ping the bot mofo')
 async def ping(ctx):
-	"""Bot Is dead"""
-	await ctx.send(f'Pong! {round(client.latency*1000)}ms')
+    """Bot Is dead"""
+    await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
 client.run(os.environ['BOT_TOKEN'])
