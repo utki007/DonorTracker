58d61adcca98a36f5e72459b37807c5271798e20
Utkarsh Narain <utkarshnarain007@gmail.com>
2021-04-06 16:12:55 +0530

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -68,8 +68,8 @@ class utils(commands.Cog):
         embed = discord.Embed(color=0x02ff06, description=f'The {channel.name} is Lock for {role.mention}')
         await channel.send(embed=embed)
         role = discord.utils.get(ctx.guild.roles, name="blue")
-        await ctx.send(f'{role.mention}}')
-        await ctx.send(f'{role.name}}')
+        await ctx.send(f'{role.mention}')
+        await ctx.send(f'{role.name}')
 
 
 def setup(client):
